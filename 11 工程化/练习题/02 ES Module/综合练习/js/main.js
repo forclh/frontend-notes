2680b497be1fe6c3def359c8f374ebdb69eadb9f
AidenKao <forclh@qq.com>
2024-08-06 08:59:11 +0800

    updata

--- 11 工程化/练习题/02 ES Module/综合练习/js/main.js
+++ 11 工程化/练习题/02 ES Module/综合练习/js/main.js
@@ -1,3 +1,9 @@
-// 入口文件
-import { login } from "./api/user.js";
+// 入口文件，编写事件
+import login from "./login.js";
+import { formContainer } from './doms.js';
+
+formContainer.addEventListener('submit', (e) => {
+  e.preventDefault(); // 组织刷新的默认行为
+  login();
+});
 
