f1fe30d5255573e1761cc7257536ca87077472c7
AidenKao <forclh@qq.com>
2024-08-07 21:52:20 +0800

    update

---
+++ 12 Vue入门到实战/课堂代码/05 搭建工程/my-site/src/components/Avatar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img class="avatar-img" :src="url" :style="{ width: size + 'px', height: size + 'px' }">
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    url: {
+      type: String,  // url属性必须是一个字符串
+      require: true
+    },
+    size: {
+      type: Number,
+      default: 150, // 默认值
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.avatar-img {
+  border-radius: 50%;
+  object-fit: cover;
+  display: block;
+}
+</style>
---
+++ 12 Vue入门到实战/课堂代码/05 搭建工程/my-site/src/main.js
@@ -0,0 +1,6 @@
+// 入口文件
+import Vue from 'vue';
+import App from './App.vue';
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
